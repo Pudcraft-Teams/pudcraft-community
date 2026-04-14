219b0375ff847d77ff6485708af4be0be7e2ce80
HePudding <hepudding@gmail.com>
2026-04-14 22:11:51 +0800

    refactor: remove forum application layer

--- src/app/c/[slug]/layout.tsx
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from "react";
-import { ComposeProvider } from "@/components/forum/ComposeDialog";
-
-export const dynamic = "force-dynamic";
-
-export default function CircleLayout({ children }: { children: ReactNode }) {
-  return <ComposeProvider>{children}</ComposeProvider>;
-}
